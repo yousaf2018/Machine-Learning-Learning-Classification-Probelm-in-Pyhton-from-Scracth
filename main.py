1a265dfc86a8a8c15e5b5f70f7adb428c8f25600
yousaf2018 <mahmoodyousaf975974@gmail.com>
2020-12-23 07:54:54 +0500

    Project is completed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt 
+import numpy as np
 #class for machine learning probelm classification 
 class AI_model():
     #Training data with x,y,outputlabel
     training_data = [[1,1,1],[9.4,6.4,1],[2.5,2.1,1],[8,7.7,-1],[0.5,2.2,-1],[7.9,8.4,-1],[7,7,-1],[2.8,0.8,1],[1.2,3,-1],[7.8,6.1,1]]
     #Initial weights for machine learning model
-    w_old = [-1, -1, -1]
+    w_old =  [-0.6, 0.75, 0.5]
     #Learning rate
     l_r = 0.02
     #function for prediction on basis on input x,y
@@ -11,7 +13,7 @@ class AI_model():
         w1_x = self.w_old[0] * self.training_data[index][0]
         w2_y = self.w_old[1] * self.training_data[index][1]
         wo_x0 = self.w_old[2] * 1
-        sum = w1_x + w2_y + wo_x0
+        sum = w1_x - w2_y + wo_x0
         if sum >= 0:
             return 1
         else:
@@ -22,6 +24,8 @@ class AI_model():
         self.w_old[2] = self.w_old[2] + self.l_r*(self.training_data[index][2]-Actual)*self.training_data[index][2]
 if __name__ == "__main__":
     AI_model = AI_model()
+    x = [1,9.4,2.5,8,0.5,7.9,7,2.8,1.2,7.8]
+    y = [1,6.4,2.1,7.7,2.2,8.4,7,0.8,3,6.1]
     #30 epocchs for training
     for j in range(30):
         print("**************************************************")
@@ -32,6 +36,14 @@ if __name__ == "__main__":
             else:
                 AI_model.learning(i,check)
                 print("Happy Learning")
+        print(AI_model.w_old[0],AI_model.w_old[1],AI_model.w_old[2])
         print("**************************************************")
-
-
+    plt.scatter(x,y)
+    #Intercept
+    i= AI_model.w_old[2]/AI_model.w_old[1]
+    #Slope
+    s= AI_model.w_old[0]/AI_model.w_old[1]
+    # from 1 to 10, by 50
+    x=np.linspace(1,10,50)      
+    plt.plot(x, s*x + i)       
+    plt.show()
